8b8a1936aacb6aad86d24c55245e1da276f3975a
Ryan Jensen <jryan7434@gmail.com>
2025-11-16 20:42:24 -0600

    added better logging

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,4 +1,11 @@
+import json
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 def lambda_handler(event, context):
+    logger.info("Lambda invoked with event: %s", event)
     return {
         "statusCode": 200,
         "body": "Hello from my first AWS Lambda deployed via GitHub Actions"
